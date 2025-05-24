b900d0eb1ad0daa97bc84ba6e97edd3bde3e9bf8
Aman-S-07 <amansahu7431@gmail.com>
2025-05-24 19:24:35 +0530

    static files

--- Hello/settings.py
+++ Hello/settings.py
@@ -178,3 +178,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 ALLOWED_HOSTS = ['*']
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # or similar folder
